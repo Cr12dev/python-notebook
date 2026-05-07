9e70de324c29c2bade29c3ac0a4a94c2ece5f76b
Cr12dev <cristianmupc2025@hotmail.com>
2026-05-07 16:18:53 +0200

    feat: new notebook for sqlite3

--- airbnb_valencia/scraping.py
+++ airbnb_valencia/scraping.py
@@ -7,25 +7,33 @@ st.title("""
 """)
 
 file_uploader = st.file_uploader("Choose a CSV file")
+# Para mejorar la forma automatica
+url_uploader = st.text_input("Upload via URL")
 
-if file_uploader is not None:
-    # 1. Cargar el DataFrame desde el archivo subido
-    df = pd.read_csv(file_uploader)
+if file_uploader is not None or url_uploader:
+
+    # 1. Cargar el DataFrame
+    if file_uploader is not None:
+        df = pd.read_csv(file_uploader)
+    else:
+        df = pd.read_csv(url_uploader)
 
     # 2. Limpiar y convertir la columna 'price' a número
     df["price"] = (
         df["price"]
+        .astype(str)
         .str.replace('$', '', regex=False)
         .str.replace(',', '', regex=False)
-        .astype(float)
     )
+
+    df["price"] = pd.to_numeric(df["price"], errors="coerce")
 
     # 3. Agrupar y calcular la media
     promedios = df.groupby("neighbourhood")["price"].mean()
     promedios_ordenados = promedios.sort_values(ascending=False)
 
     st.write("Analyze all Airbnb data for the city of Valencia")
-    st.line_chart(promedios_ordenados)
+    st.bar_chart(promedios_ordenados)
 
 else:
-    st.write("Error: insert a valid file")
+    st.write("Error: insert a valid file or URL")
