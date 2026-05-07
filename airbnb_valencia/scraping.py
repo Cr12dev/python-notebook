b309dceb3fc1477c5bc6a62d0e4aefa79fc6c421
Cr12dev <cristianmupc2025@hotmail.com>
2026-05-07 15:23:02 +0200

    feat: add a UI using streamlit

--- airbnb_valencia/scraping.py
+++ airbnb_valencia/scraping.py
@@ -1,14 +1,31 @@
 import pandas as pd
+import streamlit as st
 
-# 1. Cargar el DataFrame
-df = pd.read_csv("./listings.csv")
+# Creacion de la UI
+st.title("""
+    Valencia Neighbourhood Airbnb scraping
+""")
 
-# 2. Limpiar y convertir la columna 'price' a número
-df["price"] = df["price"].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
+file_uploader = st.file_uploader("Choose a CSV file")
 
-# 3. Agrupar y calcular la media
-promedios = df.groupby("neighbourhood")["price"].mean()
-promedios_ordenados = promedios.sort_values(ascending=False)
+if file_uploader is not None:
+    # 1. Cargar el DataFrame desde el archivo subido
+    df = pd.read_csv(file_uploader)
 
+    # 2. Limpiar y convertir la columna 'price' a número
+    df["price"] = (
+        df["price"]
+        .str.replace('$', '', regex=False)
+        .str.replace(',', '', regex=False)
+        .astype(float)
+    )
 
-print(promedios_ordenados)
+    # 3. Agrupar y calcular la media
+    promedios = df.groupby("neighbourhood")["price"].mean()
+    promedios_ordenados = promedios.sort_values(ascending=False)
+
+    st.write("Analyze all Airbnb data for the city of Valencia")
+    st.line_chart(promedios_ordenados)
+
+else:
+    st.write("Error: insert a valid file")
